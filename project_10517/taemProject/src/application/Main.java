bd824b3dc763a1f7c598e5a943dbce4ad3dc3538
gmlxo <ys2110117@y-y.hs.kr>
2021-11-02 14:27:47 +0900

    Create Main.java

---
+++ project_10517/taemProject/src/application/Main.java
@@ -0,0 +1,30 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+//import javafx.scene.layout.AnchorPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(getClass().getResource("/Game/MainGame.fxml"));
+			//AnchorPane ap = (AnchorPane) loader.load();
+			SplitPane ap = (SplitPane) loader.load();
+			Scene scene = new Scene(ap, 600, 650);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
