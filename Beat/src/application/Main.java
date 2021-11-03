1b032aaab5f65ab6e2cd1d8dfa662542ae6f5984
gmlxo <ys2110117@y-y.hs.kr>
2021-11-03 12:43:53 +0900

    Create Main.java

---
+++ Beat/src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+public class Main {
+
+	public static final int screen_width = 1280;
+	public static final int screen_hight = 720;
+	
+	public static void main(String[] args) {
+		
+		new DynamicBeat();
+
+	}
+
+}
